9a91e87a7a1af66b6a3d067da822ea8b7a3f4de0
Ahad Iftikhar <ahadiftikhar77@gmail.com>
2025-01-14 23:36:54 +0500

    add browserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,11 @@ import { AddCar } from "./components/AddCar.jsx";
 import Protected from "./components/auth/AuthLayout";
 import { CarDetails } from "./components/CarDetails.jsx";
 import { Provider } from "react-redux";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import { store } from "./store/store.js";
 
 const router = createBrowserRouter([
@@ -67,8 +71,10 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
